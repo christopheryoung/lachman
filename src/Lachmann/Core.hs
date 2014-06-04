1aad04e066c0cb24865397a1e8f396631fd35cb8
Christopher Young <chris@chrisyoung.net>
2014-06-04 06:54:49 -0400

    Add sketch for a new approach

--- src/Lachmann/Core.hs
+++ src/Lachmann/Core.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings, NamedFieldPuns #-}
+
 module Lachmann.Core where
 
+import Control.Monad.Random
 import Data.Text as T
-
+import Data.Graph.Inductive
 
 type MSEdge = (T.Text, T.Text)
 
@@ -19,3 +22,41 @@ data Manuscript = Manuscript {
   -- ,_description :: T.Text
   ,text :: T.Text
   } deriving (Show, Eq, Ord)
+
+
+data ToyMS = ToyMS {
+  toyms_id :: T.Text
+               }
+
+instance Show ToyMS where
+  show = T.unpack . toyms_id
+
+type ToyTrad = Gr ToyMS CopyRelation
+
+-- type ToyTrad = ToyTrad {
+--   toy_edges :: [CopyRelation]
+--   ,toy_mss :: [ToyMS]
+--   }
+
+type CopyRelation = (T.Text, T.Text)
+
+data CopySpec = CopySpec {
+  copies :: Int
+                         }
+
+--instance Graph ToyTrad ToyMS()
+
+
+-- mkTraditionGraph :: MonadRandom m => ToyMS -> CopySpec -> m ToyTrad
+-- mkTraditionGraph archetype spec = do $ mkGraph nodes edges where
+--                                     m1 = (1, ToyMS "foo")
+--                                     m2 = (2, ToyMS "bar")
+
+
+
+-- mkTraditionGraph :: Tradition -> Gr Manuscript Int
+-- mkTraditionGraph trad = Data.Graph.Inductive.mkGraph (mss trad) (edgelist trad)
+
+g :: Gr String Int
+g = ([],1,"a",[]) & Data.Graph.Inductive.empty
+
